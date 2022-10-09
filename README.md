29290c431d9300a22f6f289304443ba75b7e1f53
chaechae <dokailove6326@gmail.com>
2022-10-10 07:40:00 +0900

    assignment1

--- README.md
+++ README.md
@@ -10,8 +10,13 @@
 
 ### Assignment1
 * 프로젝트의 실행 방법
-    1. 
-* 데모영상 
+    1. 이메일 / 로그인 정보 받아오기 (e.target.value)
+    2. 받아온 정보를 통해 error를 보여주기
+        * 아무 값도 입력하지 않았을 때
+        * 정규식에 맞지 않을 때
+    3. error가 없을 경우 submit 버튼을 활성화 시켜주기
+* 데모영상
+    ![Assignment1](./public/images/assignment1.gif)
 
 ### Assignment2
 * 프로젝트의 실행 방법
--- src/components/emailForm.jsx
+++ src/components/emailForm.jsx
@@ -1,18 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function EmailForm({ txt }) {
+
+    const [disabled, setDisabled] = useState(true);
+    const [error, setError] = useState({
+        email : "이메일을 입력하세요",
+        password : "비밀번호를 입력하세요",
+    });
+
+    useEffect(()=>{
+        console.log("email",error.email);
+        console.log("password",error.password);
+        if(!error.email.trim() && !error.password.trim()){
+            setDisabled(false);
+        }
+    },[error])
+
+    const onErrorEmail = (e) => {
+        const emailRegex = /^.*(@).*/
+        
+        if(!e.target.value){
+            setError({
+                ...error,
+                email : "이메일을 입력하세요"
+            })
+        }else if(!emailRegex.test(e.target.value)){
+            setError({
+                ...error,
+                email : "이메일 양식에 맞춰 입력하세요"
+            })
+        }else{
+            setError({
+                ...error,
+                email : ` `
+            })
+        }
+    }
+
+    const onErrorPassword = (e) => {
+        const passwordRegex = /^[a-zA-Z0-9]{8,}$/
+
+        if(!e.target.value){
+            setError({
+                ...error,
+                password : "비밀번호를 입력하세요"
+            })
+        }else if(!passwordRegex.test(e.target.value)){
+            setError({
+                ...error,
+                password : "최소 8글자 이상 입력하세요"
+            })
+        }else{
+            setError({
+                ...error,
+                password : ` `
+            })
+        }
+    }
+
     return (
         <form>
             <label>이메일
-            <input type="email"/>
+            <input 
+                type="email" 
+                onChange={onErrorEmail} 
+            />
             </label>
+            <div>{error.email}</div>
             <br />
             <label>비밀번호 
-            <input type="password"/>
+            <input 
+                type="password" 
+                onChange={onErrorPassword}
+            />
             </label>
+            <div>{error.password}</div>
             <br />
-            <br />
-            <button type='submit'>{txt}</button>
+            <button type='submit' disabled={disabled} >{txt}</button>
         </form>
     ) 
 }
